fb207610916965eceba3b403b7800125e90a2a1c
Hind-BENNABI <yousra.hind.bennabi@ensia.edu.dz>
2026-08-16 15:23:18 +0100

    Clean up image data structure in creative.ts
    Refactored image objects by removing unnecessary comments and adjusting formatting for consistency.

--- data/creative.ts
+++ data/creative.ts
@@ -26,15 +26,15 @@ export const creativeCategories: CreativeCategory[] = [
       //   src: "/images/projects/creative/canvas/rassm.jpg",
       //   alt: "Watercolor illustration",
       // },
-            {
+      {
         src: "/images/projects/creative/canvas/ceramic.jpg",
         alt: "Ceramic artwork",
       },
-            {
+      {
         src: "/images/projects/creative/canvas/drawing.jpg",
         alt: "Drawing",
       },
-            {
+      {
         src: "/images/projects/creative/canvas/scratch.png",
         alt: "Scratch art",
       },
@@ -67,7 +67,7 @@ export const creativeCategories: CreativeCategory[] = [
         src: "/images/projects/creative/fiber/bookmarks_crochet.jpg",
         alt: "Crochet bookmark",
       }, 
-        {
+      {
         src: "/images/projects/creative/fiber/pendant_broderie.jpg",
         alt: "crochet ",
       },
@@ -75,27 +75,27 @@ export const creativeCategories: CreativeCategory[] = [
         src: "/images/projects/creative/fiber/pillow_granny.png",
         alt: "granny squares",
       },
-         {
+      {
         src: "/images/projects/creative/fiber/octopus.jpg",
         alt: "crochet octopus",
       },
-       {
+      {
         src: "/images/projects/creative/fiber/tricot_bonnet.jpg",
         alt: "knitting",
       },
-       {
+      {
         src: "/images/projects/creative/fiber/tricot_mitaines.jpg",
         alt: "crochet",
       },
-      //  {
+      // {
       //   src: "/images/projects/creative/fiber/broderie_seau.jpg",
       //   alt: "embroidery",
       // },
-       {
+      {
         src: "/images/projects/creative/fiber/broderie_licorne.jpg",
         alt: "broderie",
       },
-       {
+      {
         src: "/images/projects/creative/fiber/bag_beige_macrame.jpg",
         alt: "macrame",
       },
@@ -111,7 +111,7 @@ export const creativeCategories: CreativeCategory[] = [
         src: "/images/projects/creative/fiber/bluenwhite_sac.jpg",
         alt: "macramé bag" 
       },
-      //       {
+      // {
       //   src: "/images/projects/creative/fiber/beige_bag.jpg",
       //   alt: "macramé bag",
       // },
@@ -122,35 +122,37 @@ export const creativeCategories: CreativeCategory[] = [
       {
         src: "/images/projects/creative/fiber/bresilien.jpg",
         alt: "Bracelet Bresilien",
-      },      {
+      },      
+      {
         src: "/images/projects/creative/fiber/chaussons.jpg",
         alt: "Crochet baby booties", 
       },
       {
-                src: "/images/projects/creative/fiber/echarpe.jpg",
+        src: "/images/projects/creative/fiber/echarpe.jpg",
         alt: "knitted scarf",
-      },      {
+      },      
+      {
         src: "/images/projects/creative/fiber/glasses.jpg",
         alt: "Crochet glass holder ", 
       },
-        {
+      {
         src: "/images/projects/creative/fiber/gourde.jpg",
         alt: "Crochet water bottle holder", 
       },
-       {
+      {
         src: "/images/projects/creative/fiber/paperou.png",
         alt: "Crochet paperou ", 
       },
-        {
+      {
         src: "/images/projects/creative/fiber/red_rose.png",
         alt: "Crochet bag", 
       },
-        {
+      {
         src: "/images/projects/creative/fiber/suspension.jpg",
         alt: "macramé suspension", 
       },
-       {
-        src: "/images/projects/creative/fiber/mirror.jpg,
+      {
+        src: "/images/projects/creative/fiber/mirror.jpg",
         alt: "macramé suspension", 
       },
     ],
@@ -166,7 +168,6 @@ export const creativeCategories: CreativeCategory[] = [
     bg: "green",
 
     images: [
-     
       {
         src: "/images/projects/creative/lifestyle/collier_beads.jpg",
         alt: "beads necklace",
@@ -179,7 +180,7 @@ export const creativeCategories: CreativeCategory[] = [
         src: "/images/projects/creative/lifestyle/verrine.jpg",
         alt: "dessert",
       },
-            {
+      {
         src: "/images/projects/creative/lifestyle/cakeN.jpg",
         alt: "cake",
       },
@@ -187,39 +188,39 @@ export const creativeCategories: CreativeCategory[] = [
         src: "/images/projects/creative/lifestyle/3rdyear.jpg",
         alt: "dessert",
       },
-            {
+      {
         src: "/images/projects/creative/lifestyle/collier_coquillage.jpg",
         alt: "necklace",
       },
-       {
+      {
         src: "/images/projects/creative/lifestyle/islamic_project.jpg",
         alt: "Lifestyle",
       },
-       {
+      {
         src: "/images/projects/creative/lifestyle/musey.jpg",
         alt: "lifestyle",
       },
-       {
+      {
         src: "/images/projects/creative/lifestyle/crocodile.jpg",
         alt: "lifestyle",
       },
-            {
+      {
         src: "/images/projects/creative/lifestyle/gauffres.jpg",
         alt: "waffles",
       },
-          {
+      {
         src: "/images/projects/creative/lifestyle/souyouf.jpg",
         alt: "lifestyle",
       },
-       {
+      {
         src: "/images/projects/creative/lifestyle/physics.jpg",
         alt: "lifestyle",
       },
-       {
+      {
         src: "/images/projects/creative/lifestyle/ba9radj.jpg",
         alt: "lifestyle",
       },
-       {
+      {
         src: "/images/projects/creative/lifestyle/patisserie_heart.jpg",
         alt: "dessert",
       },
@@ -227,12 +228,10 @@ export const creativeCategories: CreativeCategory[] = [
         src: "/images/projects/creative/lifestyle/project_science.jpg",
         alt: "science",
       },
-       {
+      {
         src: "/images/projects/creative/lifestyle/coco.jpg",
         alt: "lifestyle",
       },
-   
-
     ],
   },
 
@@ -250,30 +249,36 @@ export const creativeCategories: CreativeCategory[] = [
         src: "/images/projects/creative/certificates/bac_wilaya.jpg",
         alt: "Certificate",
       },     
-       {
+      {
         src: "/images/projects/creative/certificates/bac.jpg",
         alt: "Certificate",
-      }, {
+      }, 
+      {
         src: "/images/projects/creative/certificates/competition_lycee.jpg",
         alt: "Certificate",
-      },      {
+      },      
+      {
         src: "/images/projects/creative/certificates/eng_4am.jpg",
         alt: "Certificate",
-      },      {
+      },      
+      {
         src: "/images/projects/creative/certificates/islamic.jpg",
         alt: "Certificate",
-        
       }, 
-  {
+      {
         src: "/images/projects/creative/certificates/1as.jpg",
-        alt: "Certificate", }, {
-       src: "/images/projects/creative/certificates/writer.jpg",
+        alt: "Certificate",
+      }, 
+      {
+        src: "/images/projects/creative/certificates/writer.jpg",
         alt: "writer certificate",
-      },  {
+      },  
+      {
         src: "/images/projects/creative/certificates/islamic2.jpg",
         alt: "Certificate",
-      },      {
-       src: "/images/projects/creative/certificates/soroban_participation.jpg",
+      },      
+      {
+        src: "/images/projects/creative/certificates/soroban_participation.jpg",
         alt: "soroban certificate",
       },       
       {
@@ -284,11 +289,11 @@ export const creativeCategories: CreativeCategory[] = [
         src: "/images/projects/creative/certificates/soroban2.jpg",
         alt: "soroban certificate",
       },
-       {
+      {
         src: "/images/projects/creative/certificates/hajj.png",
         alt: "Certificate",
       },
-           {
+      {
         src: "/images/projects/creative/certificates/fikh.jpg",
         alt: "Certificate",
       },
